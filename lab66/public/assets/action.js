bd809dcab5b1ffcd2aea94e51cd5c1dd87c52f66
oz7ekin <sozte069@uottawa.ca>
2023-03-05 14:00:24 -0500

    lab6

---
+++ lab66/public/assets/action.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+
+  $('form').on('submit', function(){
+
+
+
+      $.ajax({
+        type: 'POST',
+        url: '/index',
+        data: $(this).serializeArray(),
+        success: function(data){
+        
+          $("#bb").css("background-color", "red");
+          $("#bb").prop("disabled", "true");
+          $("#bb").text("Thank you!");
+        }
+      });
+      return false;
+  });
+});
